738d672dd75d203df451a58875945fcf96457230
Krishan Dwivedi <krishandwive9@gmail.com>
2023-10-08 12:41:56 +0530

    Added a slidable dependency

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 // ignore: must_be_immutable
 class ToDoTile extends StatelessWidget {
@@ -15,29 +16,38 @@ class ToDoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
-      child: Container(
-        padding: const EdgeInsets.all(24.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          children: [
-            //Checkbox
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.deepPurple,
-            ),
+      child: Slidable(
+        endActionPane: ActionPane(motion: StretchMotion(), children: [
+          SlidableAction(
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            onPressed: null,
+          ),
+        ]),
+        child: Container(
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              //Checkbox
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.deepPurple,
+              ),
 
-            //Task name
-            Text(
-              taskName,
-              style: TextStyle(
-                  decoration:
-                      taskCompleted ? TextDecoration.lineThrough : null),
-            ),
-          ],
+              //Task name
+              Text(
+                taskName,
+                style: TextStyle(
+                    decoration:
+                        taskCompleted ? TextDecoration.lineThrough : null),
+              ),
+            ],
+          ),
         ),
       ),
     );
